c0e6ee13cb2a7d67eaeaef9fd99ce867aef28b39
Narazaka <info@narazaka.net>
2017-12-03 01:03:48 +0900

    ボックスなど

---
+++ src/components/ItemContent.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+export const ItemContent = ({children}: {children: React.ReactNode}) =>
+    <div className="ItemContent">
+        {children}
+    </div>;
--- src/components/SkillContent.tsx
+++ src/components/SkillContent.tsx
@@ -1,9 +1,13 @@
 import { observer } from "mobx-react";
 import * as React from "react";
+import { ItemContent } from "./ItemContent";
+import { ItemPropertyBox } from "./ItemPropertyBox";
 import { Skill } from "./state/Skill";
 
 export const SkillContent = observer(({skill}: {skill: Skill}) =>
-    <div className="ItemContent">
-        {skill.id}
-    </div>,
+    <ItemContent>
+        <ItemPropertyBox title="基本設定">
+            {skill.name}
+        </ItemPropertyBox>
+    </ItemContent>,
 );
