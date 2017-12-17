dba9d49c351145a47076e8bd885b0f20dba12ec9
Narazaka <info@narazaka.net>
2017-12-17 18:59:27 +0900

    ItemBaseを汎用に分離

---
+++ src/components/state/ItemAnyBase.ts
@@ -0,0 +1,29 @@
+import { observable } from "mobx";
+
+export class ItemAnyBase<ItemContent> {
+    @observable private _item: ItemContent;
+
+    constructor(item: {[name in keyof ItemContent]: ItemContent[name]}) {
+        this._item = item;
+
+        return new Proxy(this, {
+            get(target, name) {
+                return (target._item as any)[name];
+            },
+            set(target, name, value) {
+                (target._item as any)[name] = value;
+
+                return true;
+            },
+            has(target, name) {
+                return name in (target._item as any);
+            },
+            ownKeys(target) {
+                return Object.keys(target._item);
+            },
+            getOwnPropertyDescriptor(target, name) {
+                return Object.getOwnPropertyDescriptor(target._item, name);
+            },
+        });
+    }
+}
--- src/components/state/ItemBase.ts
+++ src/components/state/ItemBase.ts
@@ -1,37 +1,11 @@
-import { observable } from "mobx";
+import { ItemAnyBase } from "./ItemAnyBase";
 
 export interface ItemContentBase {
     id: number;
     name?: string;
 }
 
-export class ItemBase<ItemContent extends ItemContentBase> {
+export class ItemBase<ItemContent extends ItemContentBase> extends ItemAnyBase<ItemContent> {
     id: number;
     name?: string;
-
-    @observable private _item: ItemContent;
-
-    constructor(item: {[name in keyof ItemContent]: ItemContent[name]}) {
-        this._item = item;
-
-        return new Proxy(this, {
-            get(target, name) {
-                return (target._item as any)[name];
-            },
-            set(target, name, value) {
-                (target._item as any)[name] = value;
-
-                return true;
-            },
-            has(target, name) {
-                return name in (target._item as any);
-            },
-            ownKeys(target) {
-                return Object.keys(target._item);
-            },
-            getOwnPropertyDescriptor(target, name) {
-                return Object.getOwnPropertyDescriptor(target._item, name);
-            },
-        });
-    }
 }
