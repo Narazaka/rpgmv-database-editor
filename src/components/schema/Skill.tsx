88c820d55f8c567433293a54b5b7b6a80728ea39
Narazaka <info@narazaka.net>
2017-12-17 20:01:17 +0900

    色々整備

--- src/components/schema/Skill.tsx
+++ src/components/schema/Skill.tsx
@@ -79,9 +79,19 @@ export const schema = (project: Project) => withOptional({
             title: "ダメージ",
             properties: {
                 critical: { type: "boolean", title: "会心", enumNames: ["あり", "なし"] },
-                elementId: { type: "number", title: "属性" },
+                elementId: {
+                    type: "number",
+                    title: "属性",
+                    enum: [-1].concat(project.system.elements.map((_, index) => index)),
+                    enumNames: ["通常攻撃"].concat(project.system.elements.map((value) => value ? value : "なし")),
+                },
                 formula: { type: "string", title: "計算式" },
-                type: { type: "number", title: "タイプ" },
+                type: {
+                    type: "number",
+                    title: "タイプ",
+                    enum: [0, 1, 2, 3, 4, 5, 6],
+                    enumNames: ["なし", "HPダメージ", "MPダメージ", "HP回復", "MP回復", "HP吸収", "MP吸収"],
+                },
                 variance: { type: "number", title: "分散度" },
             },
         },
@@ -111,17 +121,42 @@ export const schema = (project: Project) => withOptional({
         mpCost: { type: "number", title: "消費MP" },
         name: { type: "string", title: "名前" },
         note: { type: "string", title: "メモ" },
-        occasion: { type: "number", title: "使用可能時" },
+        occasion: {
+            type: "number",
+            title: "使用可能時",
+            enum: [0, 1, 2, 3],
+            enumNames: ["常時", "バトル画面", "メニュー画面", "使用不可"],
+        },
         repeats: { type: "number", title: "連続回数" },
-        requiredWtypeId1: { type: "number", title: "武器タイプ1" },
-        requiredWtypeId2: { type: "number", title: "武器タイプ2" },
-        scope: { type: "number", title: "範囲" },
+        requiredWtypeId1: {
+            type: "number",
+            title: "武器タイプ1",
+            enum: project.system.weaponTypes.map((_, index) => index),
+            enumNames: project.system.weaponTypes.map((value) => value ? value : "なし"),
+        },
+        requiredWtypeId2: {
+            type: "number",
+            title: "武器タイプ2",
+            enum: project.system.weaponTypes.map((_, index) => index),
+            enumNames: project.system.weaponTypes.map((value) => value ? value : "なし"),
+        },
+        scope: {
+            type: "number",
+            title: "範囲",
+            enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            enumNames: [
+                "なし", "敵単体", "敵全体",
+                "敵1体ランダム", "敵2体ランダム", "敵3体ランダム", "敵4体ランダム",
+                "味方単体", "味方全体", "味方単体（戦闘不能）", "味方全体（戦闘不能）",
+                "使用者",
+            ],
+        },
         speed: { type: "number", title: "速度補正" },
         stypeId: {
             type: "number",
             title: "スキルタイプ",
-            enum: project.system.skillTypes!.map((_, index) => index),
-            enumNames: project.system.skillTypes!.map((value) => value ? value : "なし"),
+            enum: project.system.skillTypes.map((_, index) => index),
+            enumNames: project.system.skillTypes.map((value) => value ? value : "なし"),
         },
         successRate: { type: "number", title: "成功率" },
         tpCost: { type: "number", title: "消費TP" },
@@ -135,8 +170,24 @@ export const uiSchema = {
 
     "id": {"ui:widget": "hidden"},
     "damage": {
-        critical: { "ui:widget": "select" },
-        variance: { "ui:widget": "updown" },
+        "ui:field": "layout_grid",
+        "ui:layout_grid": {"ui:row": [
+            {"ui:col": {md: 12, children: [
+                {"ui:row": [
+                    {"ui:col": {md: 6, children: ["type"]}},
+                    {"ui:col": {md: 6, children: ["elementId"]}},
+                ]},
+                {"ui:row": [
+                    {"ui:col": {md: 12, children: ["formula"]}},
+                ]},
+                {"ui:row": [
+                    {"ui:col": {md: 4, children: ["variance"]}},
+                    {"ui:col": {md: 4, children: ["critical"]}},
+                ]},
+            ]}},
+        ]},
+        "critical": { "ui:widget": "select" },
+        "variance": { "ui:widget": "updown" },
     },
     "description": {"ui:widget": "textarea"},
     "message1": {"ui:options": {label: false}},
@@ -191,8 +242,24 @@ export const uiSchema = {
                     {"ui:col": {md: 12, children: ["message2"]}},
                 ]},
             ]}},
+            {"ui:group": "必要武器", "ui:col": {md: 12, children: [
+                {"ui:row": [
+                    {"ui:col": {md: 6, children: ["requiredWtypeId1"]}},
+                    {"ui:col": {md: 6, children: ["requiredWtypeId2"]}},
+                ]},
+            ]}},
         ]}},
         {"ui:col": {md: 6, children: [
+            {"ui:group": "ダメージ", "ui:col": {md: 12, children: [
+                {"ui:row": [
+                    {"ui:col": {md: 12, children: ["damage"]}},
+                ]},
+            ]}},
+            {"ui:group": "使用効果", "ui:col": {md: 12, children: [
+                {"ui:row": [
+                    {"ui:col": {md: 12, children: ["effects"]}},
+                ]},
+            ]}},
             {"ui:group": "メモ", "ui:col": {md: 12, children: [
                 {"ui:row": [
                     {"ui:col": {md: 12, children: ["note"]}},
