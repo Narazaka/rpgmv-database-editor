52af3deb8c8ac27a8cb1a290e29343b59ed6b47e
Narazaka <info@narazaka.net>
2018-05-19 20:28:20 +0900

    body

--- src/rendererMain.tsx
+++ src/rendererMain.tsx
@@ -5,6 +5,6 @@ import { App } from "./components/App";
 window.addEventListener("DOMContentLoaded", () =>
     render(
         <App />,
-        document.getElementsByTagName("body")[0],
+        document.body,
     ),
 );
